19e68ce50e84827a87241ebfa31108e3f0a03e37
MelindaKleszken <melindakleszken1021@gmail.com>
2021-02-04 22:11:53 +0000

    discover catalog sorted

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Card = ({id, name, description}) => {
-  return (
-    <div className='beer-card'>
-      <img alt='beers' src={`https://images.punkapi.com/v2/${id}?size=200x200`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{description}</p>
-      </div>
+const Card = ({beer}) => {
+ return (
+    <Container>
+    <img src={beer.image_url} alt={beer.title} />
+    <div className="beer-info">
+    <h3>{beer.name}</h3>
+    <p>{beer.description}</p>
     </div>
-  );
+    </Container>
+    )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 35vh;
+  width: 40vw;
+  border: 1px solid black;
+  margin: 3px;
+  padding: 2vw;
+  background-color: #68c1fd;
+`;
 
 export default Card;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
+import styled from 'styled-components';
+
+const Home = () =>  {   
+  return <h1>Welcome</h1>
+};
 
 
-class Home extends Component {
-  render() {
-      return (
-        
-        <div>
-          <h1>Here comes the home page</h1>
-          <div className="beer-details">
-            {this.state.loading ? (
-              <h1>loading...</h1>
-              ) : (
-            <>
-              <img src={this.state.data[1].image_url} alt={this.state.data[1].name}/>
-              <div className="beer-info">
-              <p>Name: {this.state.data[1].name}</p>
-              <p>About: {this.state.data[1].description}</p>
-              <p>Best with: {this.state.data[1].food_pairing[0]}, {this.state.data[1].food_pairing[1]}</p>
-              </div>
-              
-            </>
-              )}
-          </div>         
-        </div>
-      );
-    }
-  }
    
-  export default Home;
+export default Home;
